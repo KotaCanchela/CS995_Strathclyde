7566e31858b7109fb6bc3e38d626dfedae242f1b
KotaCanchela <dakotacanchela@gmail.com>
2020-09-26 14:02:42 +0100

    Minimise

--- Week_2/2.7 Minimisation/minimise.py
+++ Week_2/2.7 Minimisation/minimise.py
@@ -5,3 +5,15 @@ x = 1.5
 y = testFunction(x)
 
 print(f"testFunction({str(x)}) = {str(y)}")
+
+
+listNum = []
+for i in [float(j) / 100 for j in range(-199, 500)]:
+    # Adds all numbers that are -2 < x < 5 to the list called listNum
+    listNum.append(i)
+
+
+testFunctionValues = []
+for num in listNum:
+    testFunctionValues.append(testFunction(num))
+
--- Week_2/2.7 Minimisation/testFunction.py
+++ Week_2/2.7 Minimisation/testFunction.py
@@ -1,7 +1,9 @@
 import math
 
+
 def testFunction(x):
     if x < -2 or x > 5:
         return 0
-    return math.cos(x)*(x**2 - 2*x - 2.5) + (0.4*x + 2.5)
-    
+    return math.cos(x) * (x ** 2 - 2 * x - 2.5) + (0.4 * x + 2.5)
+
+
