97159b4830c1225f9801a01dc0931774ef63bd83
KotaCanchela <dakotacanchela@gmail.com>
2020-09-29 13:52:13 +0100

    Minimisation

--- Week_2/2.7 Minimisation/testFunction.py
+++ Week_2/2.7 Minimisation/testFunction.py
@@ -4,6 +4,5 @@ import math
 def testFunction(x):
     if x < -2 or x > 5:
         return 0
-    return math.cos(x) * (x ** 2 - 2 * x - 2.5) + (0.4 * x + 2.5)
-
+    return math.cos(x)*(x**2 - 2*x - 2.5) + (-0.4*x + 2.5)
 
